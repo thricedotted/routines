4f6c18cff51e02479a1edb128710f2c9174638b2
thricedotted <thricedotted@gmail.com>
2019-10-11 19:40:25 -0700

    fix fetch bug and separate caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,49 @@
-const CACHE_NAME = 'routines-cache-v1'
-const urlsToCache = [
-  '/',
-  '/global.css',
-  '/bundle.css',
-  '/bundle.js',
-  '/fonts/MaterialIcons-Regular.woff',
-  '/fonts/MaterialIcons-Regular.woff2',
-  '/fonts/work-sans-v5-latin-300.woff',
-  '/fonts/work-sans-v5-latin-regular.woff',
-  '/fonts/work-sans-v5-latin-500.woff',
-  '/fonts/work-sans-v5-latin-600.woff',
-  '/fonts/work-sans-v5-latin-700.woff',
-  '/fonts/work-sans-v5-latin-300.woff2',
-  '/fonts/work-sans-v5-latin-regular.woff2',
-  '/fonts/work-sans-v5-latin-500.woff2',
-  '/fonts/work-sans-v5-latin-600.woff2',
-  '/fonts/work-sans-v5-latin-700.woff2'
+const CACHE_GROUPS = [
+  {
+    name: 'app-cache-v1',
+    urls: [
+      '/',
+      '/global.css',
+      '/bundle.css',
+      '/bundle.js',
+      '/manifest.json'
+    ],
+  },
+
+  {
+    name: 'font-cache-v1',
+    urls: [
+      '/fonts/MaterialIcons-Regular.woff',
+      '/fonts/MaterialIcons-Regular.woff2',
+      '/fonts/work-sans-v5-latin-300.woff',
+      '/fonts/work-sans-v5-latin-regular.woff',
+      '/fonts/work-sans-v5-latin-500.woff',
+      '/fonts/work-sans-v5-latin-600.woff',
+      '/fonts/work-sans-v5-latin-700.woff',
+      '/fonts/work-sans-v5-latin-300.woff2',
+      '/fonts/work-sans-v5-latin-regular.woff2',
+      '/fonts/work-sans-v5-latin-500.woff2',
+      '/fonts/work-sans-v5-latin-600.woff2',
+      '/fonts/work-sans-v5-latin-700.woff2'
+    ]
+  }
 ]
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  async function cacheUrls() {
-    const cache = await caches.open(CACHE_NAME)
-    console.log('yay cached')
-    return cache.addAll(urlsToCache)
+self.addEventListener('install', event => {
+  async function cacheUrls(cacheGroup) {
+    const cache = await caches.open(cacheGroup.name)
+    return cache.addAll(cacheGroup.urls)
   }
 
-  event.waitUntil(cacheUrls())
+  const allCached = Promise.all(CACHE_GROUPS.map(cacheUrls))
+
+  event.waitUntil(allCached)
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   async function matchCache() {
     const response = await caches.match(event.request)
-    return response ? response : event.request
+    return response ? response : fetch(event.request)
   }
 
   event.respondWith(matchCache())
